08dbc72b65221244aa4824554d501af4c55174d0
salindae25 <salindae25@gmail.com>
2018-08-09 08:55:06 +0530

    ui changes to places component

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -37,13 +37,14 @@ export class PlacesComponent implements OnInit {
 
   ngOnInit() {
     this.types = [
-      { DataAvaialble: false, Checked: false, Name: 'Restaurant', ParameterName: 'restaurant' },
       { DataAvaialble: false, Checked: false, Name: 'Cafe', ParameterName: 'cafe' },
+      { DataAvaialble: false, Checked: false, Name: 'School', ParameterName: 'school' },
       { DataAvaialble: false, Checked: false, Name: 'Hotel', ParameterName: 'lodging' },
-      { DataAvaialble: false, Checked: false, Name: 'Courthouse', ParameterName: 'courthouse' },
+      { DataAvaialble: false, Checked: false, Name: 'Hospitalm', ParameterName: 'hospital' },
       { DataAvaialble: false, Checked: false, Name: 'Gym', ParameterName: 'gym' },
-      { DataAvaialble: false, Checked: false, Name: 'ATM', ParameterName: 'atm' },
-      { DataAvaialble: false, Checked: false, Name: 'Bank', ParameterName: 'bank' }
+      { DataAvaialble: false, Checked: false, Name: 'Spa', ParameterName: 'spa' },
+      { DataAvaialble: false, Checked: false, Name: 'Restaurant', ParameterName: 'restaurant' },
+
     ];
   }
 
